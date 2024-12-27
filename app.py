6c5cc5740657d8eaba5e027f8a83901fa4630cb5
Nopass0 <gal.bogdan2015@gmail.com>
2024-12-27 22:33:19 +0500

    new

--- app.py
+++ app.py
@@ -13,22 +13,30 @@ def get_orders():
 
     token = token.replace('\n', '')
 
+    print(token)
+
     with open('token.txt', 'w') as f:
         f.write(token)
 
     w = Wallet.token_from_file('token.txt')
     orders = w.get_own_p2p_order_history(0, 100, "COMPLETED_FOR_REQUESTER")
 
+    print(orders)
+
     orders_json = []
     for order in orders:
         order_info = {
             "order_id": order.id,
             "amount": {
-                "value": order.amount.amount,
-                "currency_code": order.amount.currencyCode
+                "value": order.amount.amount if order.amount else None,
+                "currency_code": order.amount.currencyCode if order.amount else None
             },
-            "buyer_id": order.buyer.userId,
-            "seller_id": order.seller.userId,
+            "volume": {
+                "value": order.volume.amount if order.volume else None,
+                "currency_code": order.volume.currencyCode if order.volume else None
+            },
+            "buyer_id": order.buyer.userId if order.buyer else None,
+            "seller_id": order.seller.userId if order.seller else None,
             "payment_method": order.paymentDetails.paymentMethod.name if order.paymentDetails and order.paymentDetails.paymentMethod else None,
             "status": order.status,
             "status_update_time": order.statusUpdateDateTime
